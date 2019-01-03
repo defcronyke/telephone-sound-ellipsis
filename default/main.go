3b52a3acc66f6e8e20ab60a6ca4d009bfeb35662
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-01-03 00:18:48 -0500

    redirect to new version of camera filter

--- default/main.go
+++ default/main.go
@@ -2,12 +2,15 @@ package default_module
 
 import (
 	"net/http"
-
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
 )
 
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://defcronyke.github.io/ts-camera-filter", http.StatusFound)
+}
+
 func init() {
-	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-	http.Handle("/", r)
+	// r := mux.NewRouter()
+	// r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	http.HandleFunc("/", redirect)
 }
